79ed1eb5138dced9e5af9c6e65b146b121fd962c
adishap <porwaladisha@gmail.com>
2015-01-27 15:53:40 +0530

    added register_team.php

--- DatabaseDetails.md
+++ DatabaseDetails.md
@@ -1,23 +1,23 @@
 # Database Details:
 
 ***project_aspirants***
-	*team_id
-	*project_title
-	*abstract
-	*password
+	*team_id	(varchar,20)
+	*project_title	(varchar,100)
+	*abstract	(varchar,500)
+	*password	(varchar,35)
 
 ***peers***
-	*name	
-	*roll_no
-	*website_link
-	*password
+	*name	(varchar,25)
+	*roll_no 	(varchar,20)
+	*website_link	(varchar,150)
+	*password	(varchar,35)
 
 ***requested_peers***
-	*team_id
-	*peer_roll_no
+	*team_id	(varchar,20)
+	*peer_roll_no	(varchar,20)
 
 ***allocated_teams***
-	*peer_roll_no
-	*aspirant_id1
-	*aspirant_id2
-	*aspirant_id3
+	*peer_roll_no	(varchar,20)	
+	*aspirant_id1	(varchar,20)
+	*aspirant_id2	(varchar,20)
+	*aspirant_id3	(varchar,20)
---
+++ register_team.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+  <title>Register Team</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Form Name</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="roll no">Roll number</label>  
+  <div class="col-md-4">
+  <input id="roll no" name="roll no" placeholder="Enter your Roll number" class="form-control input-md" required="" type="text">
+  <span class="help-block">Roll number of team no.1</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="roll no">Roll number</label>  
+  <div class="col-md-4">
+  <input id="roll no" name="roll no" placeholder="Enter your Roll number" class="form-control input-md" required="" type="text">
+  <span class="help-block">Roll number of team no.2</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="roll no">Roll number</label>  
+  <div class="col-md-4">
+  <input id="roll no" name="roll no" placeholder="Enter your Roll number" class="form-control input-md" required="" type="text">
+  <span class="help-block">Roll number of team no.3</span>  
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="project_title">Project tittle</label>  
+  <div class="col-md-4">
+  <input id="project_title" name="project_title" placeholder="Enter your project name" class="form-control input-md" required="" type="text">
+  <span class="help-block">Enter your project title</span>  
+  </div>
+</div>
+
+<!-- Textarea -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="abstract">Abstract</label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" id="abstract" name="abstract">project abstract</textarea>
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="new password">New password </label>
+  <div class="col-md-4">
+    <input id="new password" name="new password" placeholder="passward" class="form-control input-md" required="" type="password">
+    <span class="help-block">Enter your new passward</span>
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="confirm password">Confirm password</label>
+  <div class="col-md-4">
+    <input id="confirm password" name="confirm password" placeholder="confirm your passward" class="form-control input-md" required="" type="password">
+    <span class="help-block">Re-write your password</span>
+  </div>
+</div>
+
+</fieldset>
+</form>
+
+</body>
+</html>
