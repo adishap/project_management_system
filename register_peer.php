95a05d1c78c8db976681761c42f59189819f1a71
MY_USERNAME <MY_MAIL>
2015-01-28 15:58:13 +0530

    final ui of peer

--- register_peer.php
+++ register_peer.php
@@ -4,66 +4,110 @@
   <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
+  <div class="container">
 <form class="form-horizontal">
 <fieldset>
 
 <!-- Form Name -->
-<legend>Form Name</legend>
+<label>Peer Registration</label>
 
 <!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="name">Name</label>  
-  <div class="col-md-4">
+ <div class="row">
+
+<div class="col-md-4">
+
+  <label class="  control-label" for="name">Name</label>  
+  
   <input id="name" name="name" type="text" placeholder="Ryan sharma" class="form-control input-md" required="">
-    
-  </div>
-</div>
+    </div>
+ <div class="col-md-8">
+ </div>
+ </div>
+ <br> 
 
 <!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="roll no">Roll number</label>  
-  <div class="col-md-4">
+  <div class="row">
+
+<div class="col-md-4">
+
+  <label class="  control-label" for="roll no">Roll number</label>  
+   
   <input id="roll no" name="roll no" type="text" placeholder="Enter your roll number" class="form-control input-md" required="">
-    
-  </div>
-</div>
+  
+   </div>
+ <div class="col-md-8">
+ </div>
+ </div>
+  <br> 
+
 
 <!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="website link">Website link</label>  
-  <div class="col-md-4">
+ 
+  <div class="row">
+
+ <div class="col-md-4">
+
+  <label class="  control-label" for="website link">Website link</label>  
+   
   <input id="website link" name="website link" type="text" placeholder="Enter your website link" class="form-control input-md" required="">
-    
-  </div>
-</div>
+ </div>
+ <div class="col-md-8">
+ </div>
+ </div>
+ <br> 
+ 
 
 <!-- Password input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="new password">New password </label>
-  <div class="col-md-4">
+ 
+
+  <div class="row">
+
+ <div class="col-md-4">
+
+  <label class="  control-label" for="new password">New password </label>
+   
     <input id="new password" name="new password" type="password" placeholder="Enter your new password" class="form-control input-md" required="">
-    
   </div>
-</div>
+   
+ <div class="col-md-8">
+ </div>
+ </div>
+ <br> 
 
 <!-- Password input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="confirm passward">Confirm passward</label>
-  <div class="col-md-4">
+ 
+  <div class="row">
+
+ <div class="col-md-4">
+
+  <label class="  control-label" for="confirm passward">Confirm passward</label>
+   
     <input id="confirm passward" name="confirm passward" type="password" placeholder="Re-write your passward" class="form-control input-md" required="">
     
   </div>
-</div>
+ <div class="col-md-8">
+ </div>
+ </div>
+ <br> 
 
 <!-- Button -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="submit">Submit</label>
-  <div class="col-md-4">
-    <button id="submit" name="submit" class="btn btn-primary">Submit</button>
-  </div>
-</div>
 
-</fieldset>
-</form>
+  <div class="row">
+
+ <div class="col-md-4">
+
+ 
+  
+   
+    <button id="submit" name="submit" class="btn btn-primary">Register</button>
+ 
+ </fieldset>
+ </form>  
+ <div class="col-md-8">
+ </div>
+ </div>
+ </div> 
+
+
 </body>
 </html>
