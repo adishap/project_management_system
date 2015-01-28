08f663242a0d04f6ec15a97a9f34179b9e07c1c9
MY_USERNAME <MY_MAIL>
2015-01-28 14:44:17 +0530

    ui for register peer

---
+++ register_peer.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+  <title>Register Peer</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Form Name</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="name">Name</label>  
+  <div class="col-md-4">
+  <input id="name" name="name" type="text" placeholder="Ryan sharma" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="roll no">Roll number</label>  
+  <div class="col-md-4">
+  <input id="roll no" name="roll no" type="text" placeholder="Enter your roll number" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="website link">Website link</label>  
+  <div class="col-md-4">
+  <input id="website link" name="website link" type="text" placeholder="Enter your website link" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="new password">New password </label>
+  <div class="col-md-4">
+    <input id="new password" name="new password" type="password" placeholder="Enter your new password" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="confirm passward">Confirm passward</label>
+  <div class="col-md-4">
+    <input id="confirm passward" name="confirm passward" type="password" placeholder="Re-write your passward" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="submit">Submit</label>
+  <div class="col-md-4">
+    <button id="submit" name="submit" class="btn btn-primary">Submit</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+</body>
+</html>
